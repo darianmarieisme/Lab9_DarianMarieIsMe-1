86d3c5691c4437ab3dee71fdd29c567c8dff0233
Darian Bruce <darianmariebruce@gmail.com>
2026-03-14 22:07:50 -0400

    working on player class

--- player.py
+++ player.py
@@ -4,3 +4,13 @@ This is a class symbolizing a player
 03/14/2026'''
 
 import coin
+
+class Player:
+
+    def __init__(self,name: str):
+
+        self.__name: str = name
+
+        self.__wallet: int = 20
+
+        self.__coin = coin.Coin()
